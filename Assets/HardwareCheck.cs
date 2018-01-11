6452634c6934f06cd184b2d5e0ecb68828b15dbb
Tom Hewitt <tommhewitt@gmail.com>
2018-01-11 17:51:22 +0000

    Google Play Store Version 2.1.

---
+++ Assets/HardwareCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HardwareCheck : MonoBehaviour 
+{
+	void Start () 
+    {
+        if (!SystemInfo.supportsGyroscope)
+        {
+            SceneManager.LoadScene("Hardware Check");
+        }
+	}	
+}
